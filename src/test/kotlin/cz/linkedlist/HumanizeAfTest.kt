ef55a105efbcfade26c28b17a215de12f854bc73
Martin Macko <macko.mar@gmail.com>
2017-12-22 12:57:49 +0100

    Fixes Af Test

--- src/test/kotlin/cz/linkedlist/HumanizeAfTest.kt
+++ src/test/kotlin/cz/linkedlist/HumanizeAfTest.kt
@@ -5,47 +5,19 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.IsEqual.equalTo
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.ArgumentsProvider
-import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.provider.CsvSource
 import java.time.Duration
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.temporal.ChronoUnit
 import java.util.*
-import java.util.stream.Stream
 
 /**
  * @author Martin Macko <https://github.com/LinkedList>.
  */
 class HumanizeAfTest {
-
-    class AfArgumentsProvider : ArgumentsProvider {
-        override fun provideArguments(p0: ExtensionContext?): Stream<out Arguments> {
-            return Stream.of(
-                    Arguments.of("SECONDS", 3L, "'n paar sekondes"),
-                    Arguments.of("SECONDS", 45L, "45 sekondes"),
-                    Arguments.of("SECONDS", 48L, "'n minuut"),
-                    Arguments.of("MINUTES", 1L, "'n minuut"),
-                    Arguments.of("MINUTES", 3L, "3 minute"),
-                    Arguments.of("MINUTES", 45L, "'n uur"),
-                    Arguments.of("HOURS", 1L, "'n uur"),
-                    Arguments.of("HOURS", 3L, "3 ure"),
-                    Arguments.of("HOURS", 22L, "'n dag"),
-                    Arguments.of("DAYS", 1L, "'n dag"),
-                    Arguments.of("DAYS", 3L, "3 dae"),
-                    Arguments.of("DAYS", 26L, "'n maand"),
-                    Arguments.of("MONTHS", 1L, "'n maand"),
-                    Arguments.of("DAYS", 93L, "3 maande"),
-                    Arguments.of("DAYS", 341L, "'n jaar"),
-                    Arguments.of("YEARS", 1L, "'n jaar"),
-                    Arguments.of("YEARS", 4L, "4 jaar")
-            )
-        }
-    }
 
     @BeforeEach
     fun setUp() {
@@ -53,8 +25,25 @@ class HumanizeAfTest {
     }
 
     @ParameterizedTest(name = "{index} => minus {1} {0} = {2}")
-//    ])
-    @ArgumentsSource(AfArgumentsProvider::class)
+    @CsvSource(value = [
+        "SECONDS, 3, '''n paar sekondes'",
+        "SECONDS, 45, 45 sekondes",
+        "SECONDS, 48, '''n minuut'",
+        "MINUTES, 1, '''n minuut'",
+        "MINUTES, 3, 3 minute",
+        "MINUTES, 45, '''n uur'",
+        "HOURS, 1, '''n uur'",
+        "HOURS, 3, 3 ure",
+        "HOURS, 22, '''n dag'",
+        "DAYS, 1, '''n dag'",
+        "DAYS, 3, 3 dae",
+        "DAYS, 26, '''n maand'",
+        "MONTHS, 1, '''n maand'",
+        "DAYS, 93, 3 maande",
+        "DAYS, 341, '''n jaar'",
+        "YEARS, 1, '''n jaar'",
+        "YEARS, 4, 4 jaar"
+    ])
     fun testAf(unit: ChronoUnit, value: Long, result: String) {
         val time = LocalTime.of(12, 30, 30)
         val date = LocalDate.of(2017, 5, 13)
